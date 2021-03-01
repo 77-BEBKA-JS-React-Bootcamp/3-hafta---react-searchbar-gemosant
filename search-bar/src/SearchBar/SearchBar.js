89e7409cbe508e40ad8ff99e38d135f6a12ae5c0
gemosant <rasim.mutlu@outlook.com>
2021-03-02 01:29:53 +0300

    created search bar

---
+++ search-bar/src/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SearchBar.css'
+const SearchBar = ({keyword, setKeyword}) => {
+    return(
+        <input className="search-bar"
+            key="random1"
+            value={keyword}
+            placeholder={"Search articles"}
+            onChange={(e) => setKeyword(e.target.value)}
+        />
+    )
+}
+
+export default SearchBar
